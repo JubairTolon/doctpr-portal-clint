0696171f1b23c7a2dc0f00b9274fe7e4de96ba11
ZubayerAhammedTolon <zs.tolon0073@gmail.com>
2022-08-06 23:55:14 +0600

    link addes

--- src/Pages/Shared/PrimaryButton.js
+++ src/Pages/Shared/PrimaryButton.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const PrimaryButton = () => {
     return (
-        <button className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary"><Link to="/appointment">Get Start</Link></button>
+        <Link to="/appointment"><button className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Start</button></Link>
     );
 };
 
